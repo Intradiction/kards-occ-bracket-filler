bb8cdbfbfcae4e50717a56cd6c3e686fce93f6f7
InnovationTT <greylyngao@gmail.com>
2024-03-31 11:04:08 -0400

    first commit

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,78 @@
+import { Button } from '@mui/material';
+import React from 'react'
+import { useEffect } from 'react';
+import { useRef } from 'react'
+
+export default function Canvas() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    console.log(window.innerWidth)
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let img = new Image();
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      ctx.font = "bold 48px serif";
+      ctx.strokeText("Hello world", 500, 327);
+    }
+
+    img.onerror = (err) => {
+      console.log(err)
+    }
+
+    img.src = './KardsOCC_Clash_Bracket.png';
+
+
+  }, [])
+
+  const copyImg = () => {
+    var img = document.createElement('img');
+    img.src = canvasRef.current.toDataURL()
+    
+    var div = document.createElement('div');
+    div.contentEditable = true;
+    div.appendChild(img);
+    document.body.appendChild(div);
+    
+    // do copy
+    SelectText(div);
+    document.execCommand('Copy');
+    document.body.removeChild(div);
+  }
+
+  function SelectText(element) {
+    var doc = document;
+    if (doc.body.createTextRange) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();
+      var range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+
+  return (
+    <>
+      <Button onClick={copyImg} variant="contained">Copy to Clipboard</Button>
+      <canvas
+        ref={canvasRef}
+        height={window.innerHeight}
+        width={window.innerWidth}
+      />
+
+    </>
+
+  )
+}
---
+++ src/components/Node.jsx
@@ -0,0 +1,8 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+export default function Node() {
+  return (
+    <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+  )
+}
